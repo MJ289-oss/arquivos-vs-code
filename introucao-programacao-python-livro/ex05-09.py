c23db90e391f5cf1643096e03200a3b3ff3f9751
MJ289-oss <moisescerqueira1010@gmail.com>
2025-10-31 15:02:36 -0300

    atualização

--- introucao-programacao-python-livro/ex05-09.py
+++ introucao-programacao-python-livro/ex05-09.py
@@ -1,9 +1,11 @@
 num1 = int(input('Digite um numero: '))
 num2 = int(input('Digite outro numero: '))
 
-n = 1
+resultado = 0 
+n = 0
 
-while n <= num2:
-    n += 1 
+while n <= num1:
+    n += num2
+    resultado += 1
 
-print(f'{num1} / {num2} = {n}')
+print(f'{num1} / {num2} = {resultado - 1}')
